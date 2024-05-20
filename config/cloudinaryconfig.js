45942135855a881f6c39f895fab4c1177e29080e
thanhb2000148 <thanhb2000148@student.ctu.edu.vn>
2024-05-20 09:01:07 +0700

    update1

---
+++ config/cloudinaryconfig.js
@@ -0,0 +1,27 @@
+// config/cloudinaryConfig.js
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const multer = require('multer');
+require('dotenv').config();
+
+// Cấu hình Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+  secure: true,
+});
+
+// Đảm bảo rằng biến `storage` chỉ được khai báo một lần
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'images',
+    allowed_formats: ['jpg', 'png'],
+  },
+});
+
+// Khởi tạo Multer với cấu hình lưu trữ đã tạo
+const uploadCloud = multer({ storage });
+
+module.exports = uploadCloud;
