bd556a7a1893c5bd71be034008228ed48d0d3c0a
thanhb2000148 <thanhb2000148@student.ctu.edu.vn>
2024-05-22 09:34:54 +0700

    thêm models

---
+++ models/type_product.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var TYPE_PRODUCT = new Schema({
+  TYPE_PRODUCT_CODE: {
+    type: String
+  },
+  TYPE_PRODUCT_NAME: {
+    type: String
+  }
+});
+module.exports = mongoose.model("type_product", TYPE_PRODUCT);
