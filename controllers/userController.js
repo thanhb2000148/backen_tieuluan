72f47b5a724171563a556dd09bae9dee0ff12dc2
B2000144 <trib2000144@student.ctu.edu.vn>
2024-06-01 22:59:01 +0700

    fix xung dot

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,5 @@
 const account = require("../models/account");
 const UserModel = require("../models/user");
-const UserService = require("../services/user.service");
 const userController = {
   getAllUsers: async (req, res) => {
     try {
@@ -13,9 +12,38 @@ const userController = {
       });
     }
   },
+  getAUser: async (req, res) => {
+    try {
+      const User = await account.findById(req.params.id).populate("USER_ID");
+      res.status(200).json(User);
+    } catch (err) {
+      res.status(500).json({
+        message: err.message,
+      });
+    }
+  },
+  deleteUser: async (req, res) => {
+    try {
+      const accountToDelete = await account.findByIdAndDelete(req.params.id);
+      if (!accountToDelete) {
+        return res.status(404).json({
+          message: "account not found",
+        });
+      }
+      if (accountToDelete.USER_ID) {
+        await UserModel.findByIdAndDelete(accountToDelete.USER_ID);
+      }
+      await account.findByIdAndDelete(req.params.id);
+      res.status(200).json({ message: "User deleted" });
+    } catch (err) {
+      res.status(500).json({
+        message: err.message,
+      });
+    }
+  },
   getLoginUser: async (req, res) => {
     try {
-      const user = await UserService.getLoginUser(req.user.id);
+      const user = await account.findById(req.user.id).populate("USER_ID");
       res.status(200).json(user);
     } catch (error) {
       res.status(500).json({ err: error.message });
