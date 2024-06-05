ad7eb2a4929cdafa6b12d54771e6edb4c55e0e90
B2000144 <trib2000144@student.ctu.edu.vn>
2024-06-05 17:54:16 +0700

    update nhap kho

--- controllers/inventory_entriesController.js
+++ controllers/inventory_entriesController.js
@@ -3,11 +3,32 @@ const InventoryController = {
   addInventory_Entries: async (req, res) => {
     try {
       const newInventory_Entries =
-        await Inventory_EntriesService.addInventory_Entries();
+        await Inventory_EntriesService.addInventory_Entries(
+          req.params.id,
+          req.body.price,
+          req.body.quantity,
+          req.body.key,
+          req.body.value,
+          req.body.id_supplier,
+          req.user.id
+        );
+      const quantity = newInventory_Entries.LIST_PRODUCT_CREATED[0].QUANTITY;
+      const update = await Inventory_EntriesService.updateInventoryProduct(
+        req.params.id,
+        req.body.key,
+        req.body.value,
+        quantity
+      );
+      const updateInventoryProduct =
+        await Inventory_EntriesService.updateNumberInventoryProduct(
+          req.params.id
+        );
       res.status(200).json({
         message: "Thêm thành công",
         success: true,
-        data: newInventory_Entries,
+        data1: newInventory_Entries,
+        data2: update,
+        data3: updateInventoryProduct,
       });
     } catch (error) {
       res.status(500).json({
@@ -18,3 +39,4 @@ const InventoryController = {
     }
   },
 };
+module.exports = InventoryController;
