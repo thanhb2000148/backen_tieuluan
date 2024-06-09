95347acbf1ab5c6c5f7dfadab3e97d443c30848b
thanhb2000148 <thanhb2000148@student.ctu.edu.vn>
2024-06-09 22:19:21 +0700

    chỉnh sửa quyền get

--- router/productRouter.js
+++ router/productRouter.js
@@ -2,12 +2,11 @@ const router = require("express").Router();
 const productController = require('../controllers/productController');
 const verify = require("../middleware/verifyToken");
 
-router.get('/',verify.verityToken, productController.getProducts);
+router.get('/', productController.getProducts);
 router.get('/:id', productController.getProductById);
 router.post('/fashion', verify.verityToken, productController.createProductFashion);
 router.post('/food', verify.verityToken, productController.createProductFood);
 router.post('/phone', verify.verityToken, productController.createProductPhone);
 router.put('/:id', verify.verityToken,productController.updateProduct);
 router.delete('/:id', productController.deleteProduct);
-// router.get('/type-products', productController.getAllTypeProducts);
 module.exports = router;
