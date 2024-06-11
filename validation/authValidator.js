e1588c057af017819b18b0d4b512bc6f058b044d
B2000144 <trib2000144@student.ctu.edu.vn>
2024-06-11 15:27:55 +0700

    update validator

--- validation/authValidator.js
+++ validation/authValidator.js
@@ -7,8 +7,8 @@ const registerUserSchema = Joi.object({
   middle_name: Joi.string().required().min(2),
   email_user: Joi.string().email().required(),
   phone_number: Joi.number().required(),
-  // gender_user: Joi.string().required(),
-  // avt: Joi.string().required(),
+  gender_user: Joi.string().required(),
+  avt: Joi.string().required(),
 });
 
 const loginUserSchema = Joi.object({
