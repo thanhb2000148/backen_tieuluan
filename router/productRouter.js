de45bb8fdfd71bcb9bfa2ccf70c2a4b17f14101e
thanhb2000148 <thanhb2000148@student.ctu.edu.vn>
2024-06-27 17:22:28 +0700

    chỉnh sửa router

--- router/productRouter.js
+++ router/productRouter.js
@@ -4,6 +4,7 @@ const verify = require("../middleware/verifyToken");
 
 router.get('/', productController.getProducts);
 router.get('/search', productController.searchProducts);
+router.get("/getAll", productController.getProductsAll);
 router.get('/:id', productController.getProductById);
 router.get('/category/:id', productController.getProductsByCategory);
 router.post('/fashion', verify.verityToken, productController.createProductFashion);
@@ -11,18 +12,4 @@ router.post('/food', verify.verityToken, productController.createProductFood);
 router.post('/phone', verify.verityToken, productController.createProductPhone);
 router.put('/:id', verify.verityToken,productController.updateProduct);
 router.delete('/:id', productController.deleteProduct);
-module.exports = router;
-router.get("/", productController.getProducts);
-router.get("/getAll", productController.getProductsAll);
-router.get("/:id", productController.getProductById);
-router.get("/category/:id", productController.getProductsByCategory);
-router.post(
-  "/fashion",
-  verify.verityToken,
-  productController.createProductFashion
-);
-router.post("/food", verify.verityToken, productController.createProductFood);
-router.post("/phone", verify.verityToken, productController.createProductPhone);
-router.put("/:id", verify.verityToken, productController.updateProduct);
-router.delete("/:id", productController.deleteProduct);
 module.exports = router;
