cb2d52dc8101e28a68f34ec4934905b2abfc9cf7
thanhb2000148 <thanhb2000148@student.ctu.edu.vn>
2024-10-06 16:06:44 +0700

    làm phân quyền admin

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,26 +1,44 @@
 const jwt = require("jsonwebtoken");
+
 const verify = {
   verityToken: (req, res, next) => {
     const token = req.headers.token;
     // console.log("Token nhận được:", token); // In token ra để kiểm tra
     if (token) {
-      const accessToken = token;
-      jwt.verify(accessToken, process.env.JWT_ACCESS_KEY, (err, user) => {
+      jwt.verify(token, process.env.JWT_ACCESS_KEY, (err, user) => {
         if (err) {
           return res.status(403).json({
-            message: "token không tồn tại",
+            message: "Token không hợp lệ",
             success: false,
           });
         }
-        req.user = user;
-        next();
+        req.user = user; // Lưu thông tin người dùng vào request
+        // console.log("User đã giải mã:", user); // In ra user để kiểm tra
+
+        next(); // Chuyển sang middleware hoặc route tiếp theo
       });
     } else {
       return res.status(403).json({
-        message: "bạn chưa xác thực token",
+        message: "Bạn chưa xác thực token",
         success: false,
       });
     }
   },
+  
+  verifyTokenAdmin: (req, res, next) => {
+   verify.verityToken(req, res, () => {
+    // Kiểm tra xem người dùng có phải là admin không
+    if (req.user && req.user.admin === true) {
+      next(); // Nếu là admin, cho phép tiếp tục
+    } else {
+      console.log("admin:", req.user.admin); // In ra để kiểm tra
+      return res.status(403).json({
+        message: "Bạn không có quyền truy cập",
+        success: false,
+      });
+    }
+  });
+  },
 };
+
 module.exports = verify;
--- router/userRouter.js
+++ router/userRouter.js
@@ -3,51 +3,11 @@ const authController = require("../controllers/authControllers");
 const UserService = require("../services/user.service");
 const verify = require("../middleware/verifyToken");
 const router = require("express").Router();
-const passport = require("../config/passport");
 router.get("/userLogin", verify.verityToken, userController.getLoginUser);
-router.get("/", userController.getAllUsers);
+router.get("/", verify.verifyTokenAdmin, userController.getAllUsers);
 router.get("/:id", verify.verityToken, userController.getUserById);
 router.put('/users/:id', userController.updateUser);
 
-// // Đăng nhập bằng Google : Gửi auth URL cho frontend
-// router.get('/auth/google', (req, res) => {
-//   const googleClientId = process.env.GOOGLE_CLIENT_ID; // Sử dụng biến môi trường
-//   const redirectUri = 'http://localhost:8000/v1/user/auth/google/callback'; 
-//   const scope = 'profile email';
-//   const responseType = 'code';
-//   const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=${responseType}&client_id=${googleClientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}`;
-//   res.status(200).json({ authUrl: googleAuthUrl });
-// });
-
-// // Đăng nhập bằng Google  Xử lý callback từ Google
-// router.get('/auth/google/callback', 
-//   passport.authenticate('google', { failureRedirect: '/login', session: false }),
-//   async (req, res) => {
-//     if (!req.user) {
-//       return res.status(401).json({ message: "Authentication failed!" });
-//     }
-//     console.log('Google user info:', req.user);
-
-//     try {
-//       const data = { userId: req.user.id };
-//       const accessToken = await UserService.generateAccessToken(data); 
-      
-//       // res.redirect(`http://localhost:3000/?token=${accessToken}`);
-      
-//     res.status(200).json({
-//         accessToken,
-//         message: 'Đăng nhập Google thành công',
-//         success: true,
-//       });
-      
-//     } catch (error) {
-//       console.error(error);
-//       if (!res.headersSent) {
-//         res.status(500).json({ message: error.message });
-//       }
-//     }
-//   }
-// );
 
 
 module.exports = router;
